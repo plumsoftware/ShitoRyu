419ff0e399e03e57a5dfb7c56036003c46948a07
Slava <plumsoftwareofficial@gmail.com>
2024-05-02 23:03:15 +0600

    Создал RegistrationActivity (XML)

---
+++ app/src/main/java/com/example/shitoryu/RegistrationActivity.kt
@@ -0,0 +1,14 @@
+package com.example.shitoryu
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
