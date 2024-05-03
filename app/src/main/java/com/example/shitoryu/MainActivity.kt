f4d354303a7c3eadce23eaf7a0035e2fe5eacba8
Slava <plumsoftwareofficial@gmail.com>
2024-05-03 15:12:22 +0600

    Добавил сохранение/чтение настроек

--- app/src/main/java/com/example/shitoryu/MainActivity.kt
+++ app/src/main/java/com/example/shitoryu/MainActivity.kt
@@ -1,14 +1,44 @@
 package com.example.shitoryu
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editTextTextLogin = findViewById<EditText>(R.id.editTextTextLogin)
+        val editTextTextPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        val buttonRegister = findViewById<Button>(R.id.buttonRegister)
+
+        buttonLogin.setOnClickListener {
+            val login = editTextTextLogin.text.toString()
+            val password = editTextTextPassword.text.toString()
+
+            if (login.isNotEmpty() && password.isNotEmpty()) {
+                val sp = getSharedPreferences("user", Context.MODE_PRIVATE)
+                val l = sp.getString("login", "")
+                val p = sp.getString("password", "")
+
+                if (p.equals(password) && l.equals(login)) {
+                    // YES
+                } else {
+                    // NOT YES
+                    Toast.makeText(this, "неверный логин или пароль", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+        buttonRegister.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/shitoryu/RegistrationActivity.kt
+++ app/src/main/java/com/example/shitoryu/RegistrationActivity.kt
@@ -1,14 +1,39 @@
 package com.example.shitoryu
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val editTextTextLogin = findViewById<EditText>(R.id.editTextTextLogin)
+        val editTextTextPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val editTextTextEmail = findViewById<EditText>(R.id.editTextTextEmail)
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            val login = editTextTextLogin.text.toString()
+            val password = editTextTextPassword.text.toString()
+            val email = editTextTextEmail.text.toString()
+
+            if (login.isNotEmpty() && password.isNotEmpty() && email.isNotEmpty()) {
+                val sp = getSharedPreferences("user", Context.MODE_PRIVATE)
+                val editor: SharedPreferences.Editor = sp.edit()
+                editor.putString("login", login)
+                editor.putString("password", password)
+                editor.putString("email", email)
+                editor.apply()
+            } else {
+                Toast.makeText(this, "Заполните все данные профиля!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
